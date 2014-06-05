333f38a2f820365c99f98e0fcd8229375dd89924
Harris Miller <harrismiller08@gmail.com>
2014-06-05 17:48:40 -0600

    thank you u/PotaToss

--- routes/routes.js
+++ routes/routes.js
@@ -18,6 +18,8 @@ App.MailboxRoute = Ember.Route.extend({
 
 App.MailRoute = Em.Route.extend({
 	model: function(params) {
+		// id field is of type int, need to normalize params.message_id
+		params.message_id = parseInt(params.message_id, 10);
 		return this.modelFor('mailbox').messages.findBy('id', params.message_id);
 	}
 });
