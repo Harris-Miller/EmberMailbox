f3ec6f75515ad93c174735f775b0f8bb655ab8e0
Harris Miller <harrismiller08@gmail.com>
2014-06-06 00:06:01 -0600

    wtf is up with ember-data

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
-
 App = Ember.Application.create();
+
+App.ApplicationAdapter = DS.FixtureAdapter.extend();
 
 
 Ember.Handlebars.registerBoundHelper('date', function(date) {
--- routes/routes.js
+++ routes/routes.js
@@ -6,13 +6,16 @@ App.Router.map(function() {
 
 App.ApplicationRoute = Em.Route.extend({
 	model: function() {
-		return App.Mailbox.find();
+		//return App.Mailbox.find();
+		console.log(this.store.find('mailbox'));
+		return this.store.find('mailbox');
 	}
 });
 
 App.MailboxRoute = Ember.Route.extend({
 	model: function(params) {
-		return App.Mailbox.find(params.mailbox_id);
+		//return App.Mailbox.find(params.mailbox_id);
+		return this.store.find('mailbox', {name: params.mailbox_id});
 	}
 });
 
@@ -20,6 +23,7 @@ App.MailRoute = Em.Route.extend({
 	model: function(params) {
 		// id field is of type int, need to normalize params.message_id
 		params.message_id = parseInt(params.message_id, 10);
-		return this.modelFor('mailbox').messages.findBy('id', params.message_id);
+		//return this.modelFor('mailbox').messages.findBy('id', params.message_id);
+		return this.modelFor('mailbox').get('messages').findBy('id', params.message_id);
 	}
 });
